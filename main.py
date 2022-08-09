8cca6445c4c7b01b9439f3fdd2b576802df12066
Vloer <borisruwe@hotmail.com>
2022-08-09 18:07:57 +0200

    added 2 more playlists

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from src.reddit import Reddit
 from src.spotify import Spotify
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 if __name__ == '__main__':
     reddit = Reddit()
@@ -8,12 +12,23 @@ if __name__ == '__main__':
         post_url='https://www.reddit.com/r/DnB/comments/wj64wp/this_weeks_new_tunes_technimatic_alix_perez_as/')
     reddit.set_genre_data()
 
-    spotify_list = []
+    list_neuro = []
+    list_dancefloor = []
 
     for genre in reddit.post_genres:
-        if genre.name in ['dancefloor', 'neuro']:
-            [spotify_list.append(link) for link in genre.spotify_links]
+        if genre.name == 'neuro':
+            [list_neuro.append(link) for link in genre.spotify_links]
+        elif genre.name == 'dancefloor':
+            [list_dancefloor.append(link) for link in genre.spotify_links]
 
-    spotify = Spotify(playlist_id_to_add_to='2ers3ra7gipMNBzaiEULH9')
-    song_list = spotify.get_list_of_songs_correct_format(spotify_list)
-    spotify.add_to_playlist(song_list)
+    spotify = Spotify(playlist_id_to_add_to=os.getenv('SPOTIFY_PLAYLIST'))
+    spotify_neuro = Spotify(playlist_id_to_add_to=os.getenv('SPOTIFY_PLAYLIST_NEURO'))
+    spotify_dancefloor = Spotify(playlist_id_to_add_to=os.getenv('SPOTIFY_PLAYLIST_DANCEFLOOR'))
+
+    list_both = spotify.get_list_of_songs_correct_format(list_neuro+list_dancefloor)
+    list_neuro = spotify_neuro.get_list_of_songs_correct_format(list_neuro)
+    list_dancefloor = spotify_dancefloor.get_list_of_songs_correct_format(list_dancefloor)
+
+    spotify.add_to_playlist(list_both)
+    spotify_neuro.add_to_playlist(list_neuro)
+    spotify_dancefloor.add_to_playlist(list_dancefloor)
